8c31ec293b2dc756dac1a9a8a2b7790cbfbe9d48
Edgar Santiago Ariza Garcia <74018611+smuke123@users.noreply.github.com>
2023-11-07 14:47:11 -0500

    Fase 1 subida

---
+++ auth-front/src/main.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./routes/Login.tsx";
+import Signup from "./routes/Signup.tsx";
+import { AuthProvider } from "./auth/AuthProvider.tsx";
+import Dashboard from "./routes/Dashboard.tsx";
+import ProtectedRoute from "./routes/ProtectedRoute.tsx";
+import Profile from "./routes/Profile.tsx";
+
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/",
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/me",
+        element: <Profile />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>
+);
